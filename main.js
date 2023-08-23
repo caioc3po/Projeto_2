8fbd8594e50ffe7a978bc60a5cf1b2d682ff335e
Caio Ramalho <caioexemplo@gmail.com>
2023-08-23 14:10:47 -0300

    Fix: responsive css issues

--- main.js
+++ main.js
@@ -2,8 +2,4 @@ $(document).ready(function() {
     $('.menu-hamburguer').click(function() {
         $('nav').slideToggle();
     })
-
-    $('li').click(function() {
-        $('nav').slideToggle();
-    })
 })
